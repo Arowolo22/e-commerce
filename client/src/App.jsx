42fcc0f29112548c8ccf05fd730c32f71a8fddc6
onifade <onifadeololadefavor1@gmail.com>
2025-06-19 22:24:37 +0100

    fix issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,43 +1,38 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Home from "./pages/Home";
 import { Toaster } from "react-hot-toast";
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
-  const { pathname } = useLocation();
+   const { pathname } = useLocation();
 
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
+   useEffect(() => {
+      window.scrollTo(0, 0);
+   }, [pathname]);
 
-  return null;
+   return null;
 };
 
 const App = () => {
-  return (
-    <>
-      <Toaster />
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/" element={<Navigate to="/login" replace />} />
+   return (
+      <>
+         <Toaster />
+         <Router>
+            <ScrollToTop />
+            <Routes>
+               <Route path="/login" element={<Login />} />
+               <Route path="/register" element={<Register />} />
+               <Route path="/home" element={<Home />} />
+               <Route path="/" element={<Navigate to="/login" replace />} />
 
-          {/* <Route path="/home" element={<Home />} /> */}
-        </Routes>
-      </Router>
-      <ScrollToTop />
-    </>
-  );
+               {/* <Route path="/home" element={<Home />} /> */}
+            </Routes>
+         </Router>
+      </>
+   );
 };
 
 export default App;
