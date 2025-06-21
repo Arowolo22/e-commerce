a5893500c26cc6f1aee0a046ac7882c3ec1278dd
Arowolo22 <emmanuelolalekanarowolo@gmail.com>
2025-06-21 23:54:25 +0100

    set up some things on the cloudinary

--- server/utils/cloudinary.js
+++ server/utils/cloudinary.js
@@ -2,9 +2,11 @@ const cloudinary = require("cloudinary")
 
 cloudinary.config({
     cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
-    api_key:process.env.CLOUDINARY_API_KEY,
+    
     api_secret:process.env.CLOUDINARY_SECRET_KEY
 
 });
 
 module.exports = cloudinary;
+
+// api_key:process.env.CLOUDINARY_API_KEY,
